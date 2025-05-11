9ffe65aefbe063783b0170fd52c50dc96c6c1912
Specialistas13 <emilis.slabada@gmail.com>
2025-05-11 15:02:51 +0300

    Papildomi kreipimosi ir kopijavimo metodai

--- vector.cpp
+++ vector.cpp
@@ -10,3 +10,31 @@ Vector<T>::~Vector() {
     delete[] data;
 }
 
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector& other) {
+    if (this != &other) {
+        delete[] data;
+        size = other.size;
+        capacity = other.capacity;
+        data = new T[capacity];
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+T& Vector<T>::at(size_t index) {
+    if (index>=size){
+        throw std::out_of_range("Index out of range");
+    }
+    else {
+        return data[index];
+    }
+}
+
+template <typename T>
+T& Vector<T>::operator[](size_t index) {
+    return data[index];
+}
